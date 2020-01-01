bfad8d396934e689c5aaa0d4584f13643bbbb7c1
Meghan Stovall <meghanstovall15@augustana.edu>
2019-12-31 22:39:14 -0700

    Finish all iterations

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -16,4 +16,14 @@ class WorldCup
     end
     active_players.flatten
   end
+
+  def all_players_by_position
+    @teams.reduce({}) do |hash, team|
+      team.players.each do |player|
+        hash[player.position] = active_players_by_position(player.position)
+      end
+      hash
+    end
+  end
+  
 end
--- test/world_cup_test.rb
+++ test/world_cup_test.rb
@@ -64,4 +64,24 @@ class WorldCupTest < MiniTest::Test
 
     assert_equal [pogba], world_cup.active_players_by_position("midfielder")
   end
+
+  def test_can_sort_all_players_by_position
+    france = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    france.add_player(mbappe)
+    france.add_player(pogba)
+
+    croatia = Team.new("Croatia")
+    modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    croatia.add_player(modric)
+    croatia.add_player(vida)
+
+    world_cup = WorldCup.new(2018, [france, croatia])
+
+    expected = {"forward" => [mbappe], "midfielder" => [pogba, modric], "defender" => [vida]}
+
+    assert_equal expected, world_cup.all_players_by_position
+  end
 end
